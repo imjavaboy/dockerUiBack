30c69821febcca3b7647289a18680179ee5437f2
1159311344@qq.com <1159311344@qq.com>
2022-09-07 15:09:06 +0800

    初始化项目

---
+++ src/main/java/com/gbq/docker/uiproject/UiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.gbq.docker.uiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gbq/docker/uiproject/UiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbq.docker.uiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
